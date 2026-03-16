f15cf4c990ae693532d10c1c04d3141d628bca86
hannasini <hanna.s.hamalainen@tuni.fi>
2026-03-16 08:27:42 +0000

    1

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import joblib
 from groq import Groq
 import os
 
-os.environ["GROQ_API_KEY"]= "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"
+os.environ["GROQ_API_KEY"]= ""
 client = Groq()
 
 model = joblib.load("DBS_SGD_model.pkl")
